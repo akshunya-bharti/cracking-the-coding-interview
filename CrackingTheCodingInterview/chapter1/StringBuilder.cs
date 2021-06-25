bab87785395d0dc787b9dea900fc841a1051f292
Akshunya <akshu@live.in>
2021-06-25 14:24:10 +0530

    Initial setup of book structure

--- CrackingTheCodingInterview/chapter1/StringBuilder.cs
+++ CrackingTheCodingInterview/chapter1/StringBuilder.cs
@@ -1,12 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CrackingTheCodingInterview
+namespace CrackingTheCodingInterview.chapter1
 {
-    public class Class1
+    public class StringBuilder
     {
+
     }
 }
