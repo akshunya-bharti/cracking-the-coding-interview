2a73d63c15ea71355344daea184cad8762326709
Akshunya <akshu@live.in>
2021-07-07 15:52:31 +0530

    completed basic version of stringbuilder

--- CrackingTheCodingInterview/chapter1/StringBuilder.cs
+++ CrackingTheCodingInterview/chapter1/StringBuilder.cs
@@ -1,16 +1,84 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CrackingTheCodingInterview.chapter1
 {
     public class StringBuilder
     {
-        public int AddTwo(int i)
+        private char[] container;
+        private int containerFillIndex;
+
+        public StringBuilder()
         {
-            return i + 2;
+           // Char array which holds the chars of appended string value at an instance
+           container = new char[1];
+
+           // The index value till which the container is filled at an instance
+           containerFillIndex = 0;
+        }
+
+        public void Append(string input)
+        {
+            // Check whether container length is sufficient to store new input
+            if (IsOverFlow(input))
+                ResetContainer(input.Length);
+
+            // Once the container is ready, store the input
+            InsertToStringContainer(input);
+        }
+
+        public override string ToString()
+        {
+            // Gets the string value stored in container at an instance
+            return new string(container).Substring(0, containerFillIndex);
+        }
+
+        private void InsertToStringContainer(string input)
+        {
+            char[] inputCharArray = input.ToCharArray();
+
+            // Store each character of input into the container
+            foreach (var c in inputCharArray)
+            {
+                container[containerFillIndex] = c;
+                containerFillIndex++;
+            }
+        }
+
+        private bool IsOverFlow(string input)
+        {
+            int containerLength = container.Length;
+            int inputLength = input.ToCharArray().Count();
+
+            // Check whether already stored string length along with new input length, 
+            // exceeds the current container length
+            if (containerFillIndex + inputLength > containerLength)
+                return true;
+
+            return false;
+        }
+
+        private void ResetContainer(int inputLength)
+        {
+            // Save current container chars in a temp variable
+            char[] tempContainer = container;
+
+            // Calculate new length of container
+            int containerLength = container.Length;
+            int newLength = 2 * containerLength;
+            while (newLength < inputLength + containerFillIndex)
+                newLength *= 2;
+
+            // Reset length of container to new length
+            container = new char[newLength];
+
+            // Store current container chars (saved in temp variable),
+            // back to container
+            int i = 0;
+            foreach (var c in tempContainer)
+            {
+                container[i] = c;
+                i++;
+            }
         }
     }
 }
--- CrackingTheCodingInterviewTest/chapter1/StringBuilderTest.cs
+++ CrackingTheCodingInterviewTest/chapter1/StringBuilderTest.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using CrackingTheCodingInterview.chapter1;
 using StringBuilder = CrackingTheCodingInterview.chapter1.StringBuilder;
 
 namespace CrackingTheCodingInterviewTest.chapter1
@@ -11,11 +7,89 @@ namespace CrackingTheCodingInterviewTest.chapter1
     public class StringBuilderTest
     {
         [TestMethod]
-        public void TestAddTwo()
+        public void TestEmptyStringBuilder()
         {
-            var cl = new StringBuilder();
-            var o = cl.AddTwo(3);
-            Assert.AreEqual(5, o);
+            var stringBuilder = new StringBuilder();
+            Assert.AreEqual("", stringBuilder.ToString());
+        }
+
+        [TestMethod]
+        public void TestAppendSingleChar()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("a");
+            Assert.AreEqual("a", stringBuilder.ToString());
+        }
+
+        [TestMethod]
+        public void TestAppendStringLength2()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("ab");
+            Assert.AreEqual("ab", stringBuilder.ToString());
+        }
+
+        [TestMethod]
+        public void TestAppendStringLength4()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("abcd");
+            Assert.AreEqual("abcd", stringBuilder.ToString());
+        }
+
+        [TestMethod]
+        public void TestAppendStringLength6()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("abcdef");
+            Assert.AreEqual("abcdef", stringBuilder.ToString());
+        }
+
+        [TestMethod]
+        public void TestAppendStringLength15()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("abcdeabcdeabcde");
+            Assert.AreEqual("abcdeabcdeabcde", stringBuilder.ToString());
+        }
+
+        [TestMethod]
+        public void TestAppendStringLength1And1()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("a");
+            stringBuilder.Append("a");
+            Assert.AreEqual("aa", stringBuilder.ToString());
+        }
+
+        [TestMethod]
+        public void TestAppendStringLength1And1And1()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("a");
+            stringBuilder.Append("b");
+            stringBuilder.Append("c");
+            Assert.AreEqual("abc", stringBuilder.ToString());
+        }
+
+        [TestMethod]
+        public void TestAppendStringLength4And6()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("abcd");
+            stringBuilder.Append("abcdef");
+            Assert.AreEqual("abcdabcdef", stringBuilder.ToString());
+        }
+
+        [TestMethod]
+        public void TestAppendStringLength4And6And8And1()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("abcd");
+            stringBuilder.Append("abcdef");
+            stringBuilder.Append("abcdefgh");
+            stringBuilder.Append("a");
+            Assert.AreEqual("abcdabcdefabcdefgha", stringBuilder.ToString());
         }
     }
 }
